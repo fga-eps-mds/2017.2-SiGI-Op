d56727d8188e0856405ba2b2fa44465815cf020c
Harrison <hpedro1195@gmail.com>
2017-09-17 16:18:40 -0300

    Create dropdown menu to show all ipas types to be selected

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,34 @@
   <div id="app">
     <ul>
       <li class="ipas" v-for="ipa in ipas">
-        {{ipa.id_ipa}}
-        {{ipa.name}}
-        {{ipa.institution_type}}
+        Código da IPA:{{ipa.id_ipa}}<br> 
+        Nome da IPA:{{ipa.name}}<br>
+        Tipo da IPA:{{ipa.institution_type}}<br><br>
       </li>
     </ul>
-     <div class="form">
+    <div class="form">
       <label for="ipa_id_ipa">Codigo da IPA: </label>
       <input type="number" name="ipa_id_ipa" v-model="ipa_id_ipa"><br><br>
 
-       <label for="ipa_name">Nome da IPA: </label>
+      <label for="ipa_name">Nome da IPA: </label>
       <input type="text" name="ipa_name" v-model="ipa_name"><br><br>
 
       <label for="ipa_institution_type">Tipo da IPA: </label>
-     <input type="number" name="ipa_institution_type" v-model="ipa_institution_type"><br><br>
-
+      <!-- <input type="number" name="ipa_institution_type" v-model="ipa_institution_type"><br><br> -->
+      <select v-model="ipa_type_name">
+        <option v-for="type_ipa in types_ipa" v-bind:value="type_ipa.id_type">
+          {{ type_ipa.description }}
+        </option>
+      </select>
 
       <button v-on:click.prevent="addipa">Adicionar IPA</button>
-       <button v-on:click.prevent="getipa">Lista IPA</button>
+      <button v-on:click.prevent="getipa">Lista IPA</button>
 
-
-  </div>
-    <div class="display">
-      codigo da ipa: {{ipa_id_ipa}}<br>
-      ipa: {{ipa_name}}<br>
-      tipo: {{ipa_institution_type}}
     </div>
-</div>
+    <div class="display">
+      codigo da ipa: {{ipa_id_ipa}}<br> ipa: {{ipa_name}}<br> tipo: {{ipa_type_name}}
+    </div>
+  </div>
   </div>
 </template>
 
@@ -39,9 +40,11 @@ import axios from 'axios';
 export default {
   data: () => ({
     ipas: [],
+    types_ipa: [],
     ipa_id_ipa: '',
     ipa_name: '',
     ipa_institution_type: '',
+    ipa_type_name: '',
     // ipa: {name: this.ipa_name, code: ipa_code},
     ipalist: [],
     formpost: {
@@ -59,38 +62,51 @@ export default {
         id_ipa: this.ipa_id_ipa,
         institution_type: this.ipa_institution_type,
       })
-      .then()
-      .catch((e) => {
-        this.errors.push(e);
-      });
+        .then()
+        .catch((e) => {
+          this.errors.push(e);
+        });
+      axios.get('http://localhost:8000/ipas/')
+        .then((response) => {
+          this.ipas = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
     },
     get() {
       axios.get('http://localhost:8000/ipas/')
-      .then((response) => {
-        this.ipas = response.data;
-      })
-      .catch((e) => {
-        this.errors.push(e);
-      });
+        .then((response) => {
+          this.ipas = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+    getType() {
+      axios.get('http://localhost:8000/ipas-type/')
+        .then((response) => {
+          this.types_ipa = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
     },
     getipa() {
       this.get();
+    },
+    getTypeIpa() {
+      this.getType();
     },
     addipa() {
       this.post();
     },
   },
 
-   // Fetches posts when the component is created.
+  // Fetches posts when the component is created.
   created() {
-    axios.get('http://localhost:8000/ipas/')
-    .then((response) => {
-      // JSON responses are automatically parsed.
-      this.ipas = response.data;
-    })
-    .catch((e) => {
-      this.errors.push(e);
-    });
+    this.getipa();
+    this.getTypeIpa();
   },
 };
 </script>
@@ -109,5 +125,4 @@ export default {
   font-size: 18px;
   color: #FFFFF;
 }
-
 </style>
