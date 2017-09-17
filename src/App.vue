c213e69954492e63178a152d58522e375be39e9b
William Almeida <will.allmeida@gmail.com>
2017-09-17 15:17:32 -0300

    Create object IPA and the listing of all objects. Need to create methods that deletes and update the object

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,97 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <ul>
+      <li class="ipas" v-for="ipa in ipas">
+        {{ipa.id_ipa}}
+        {{ipa.name}}
+        {{ipa.institution_type}}
+      </li>
+    </ul>
+     <div class="form">
+      <label for="ipa_id_ipa">Codigo da IPA: </label>
+      <input type="number" name="ipa_id_ipa" v-model="ipa_id_ipa"><br><br>
+
+       <label for="ipa_name">Nome da IPA: </label>
+      <input type="text" name="ipa_name" v-model="ipa_name"><br><br>
+
+      <label for="ipa_institution_type">Tipo da IPA: </label>
+     <input type="number" name="ipa_institution_type" v-model="ipa_institution_type"><br><br>
+
+
+      <button v-on:click.prevent="addipa">Adicionar IPA</button>
+       <button v-on:click.prevent="getipa">Lista IPA</button>
+
+
+  </div>
+    <div class="display">
+      codigo da ipa: {{ipa_id_ipa}}<br>
+      ipa: {{ipa_name}}<br>
+      tipo: {{ipa_institution_type}}
+    </div>
+</div>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
-  name: 'app',
+  data: () => ({
+    ipas: [],
+    ipa_id_ipa: '',
+    ipa_name: '',
+    ipa_institution_type: '',
+    // ipa: {name: this.ipa_name, code: ipa_code},
+    ipalist: [],
+    formpost: {
+      id_ipa: this.ipa_id_ipa,
+      name: this.ipa_name,
+      institution_type: this.ipa_institution_type,
+    },
+
+    errors: [],
+  }),
+  methods: {
+    post() {
+      axios.post('http://localhost:8000/ipas/', {
+        name: this.ipa_name,
+        id_ipa: this.ipa_id_ipa,
+        institution_type: this.ipa_institution_type,
+      })
+      .then()
+      .catch((e) => {
+        this.errors.push(e);
+      });
+    },
+    get() {
+      axios.get('http://localhost:8000/ipas/')
+      .then((response) => {
+        this.ipas = response.data;
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+    },
+    getipa() {
+      this.get();
+    },
+    addipa() {
+      this.post();
+    },
+  },
+
+   // Fetches posts when the component is created.
+  created() {
+    axios.get('http://localhost:8000/ipas/')
+    .then((response) => {
+      // JSON responses are automatically parsed.
+      this.ipas = response.data;
+    })
+    .catch((e) => {
+      this.errors.push(e);
+    });
+  },
 };
 </script>
 
@@ -20,4 +104,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.ipas {
+  font-size: 18px;
+  color: #FFFFF;
+}
+
 </style>
