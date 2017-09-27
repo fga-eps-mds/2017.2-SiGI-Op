43a534db7a89438b5435e0f99371a6cba5a58ebc
Mateus Andrade <mateusandrade080@gmail.com>
2017-09-27 09:39:37 -0300

    Add user control session

---
+++ test/unit/specs/Home.spec.js
@@ -0,0 +1,15 @@
+import Home from '../../../src/components/Home';
+
+describe('Home', () => {
+  it('sets correct default data', () => {
+    expect(typeof Home.data).to.equal('function');
+    const defaultData = Home.data();
+    expect(defaultData.current_username).to.equal(null);
+  });
+
+  it('should be a current_username', () => {
+    localStorage.setItem('username', 'some_user');
+    const defaultData = Home.data();
+    expect(defaultData.current_username).to.equal('some_user');
+  });
+});
