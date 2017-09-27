3403e7141c65f1554e97ec99ab70eff8602f1fd1
Harrison Pedro <hpedro1195@gmail.com>
2017-09-26 21:41:22 -0300

    Adjust dialog and put and delete methods

--- src/components/sites/Site.vue
+++ src/components/sites/Site.vue
@@ -1,82 +1,62 @@
 <template>
-    <div id="sites">
-      <v-app id="sites">
-        <v-container>
-          <v-data-table
-          v-bind:headers="headers"
-          :items="sites"
-          class="elevation-1"
-          >
-            <template slot="items" scope="site">
-              <td class="text-xs-right">{{ site.item.name }}</td>
-              <td class="text-xs-right">
-              {{ '('.concat(site.item.lattitude).concat(',')
-              .concat(site.item.longitude).concat(')')}}
-              </td>
-              <td class="text-xs-right">{{ site.item.bandwidth }}</td>
-              <td class="text-xs-right">{{ site.item.site_type }}</td>
-              <td class="text-xs-right">{{ site.item.ipa_code }}</td>
-              <td class="text-xs-right"> 
-                <v-btn fab dark small primary
-                v-on:click.prevent="deleteSite(site.item.id)"
-                type="submit">
-                  <v-icon dark> remove </v-icon>
+  <div id="sites">
+    <v-app id="sites">
+      <v-container>
+        <v-data-table v-bind:headers="headers" :items="sites" class="elevation-1">
+          <template slot="items" scope="site">
+            <td class="text-xs-right">{{ site.item.name }}</td>
+            <td class="text-xs-right">
+              {{ '('.concat(site.item.lattitude).concat(',') .concat(site.item.longitude).concat(')')}}
+            </td>
+            <td class="text-xs-right">{{ site.item.bandwidth }}</td>
+            <td class="text-xs-right">{{ site.item.site_type }}</td>
+            <td class="text-xs-right">{{ site.item.ipa_code }}</td>
+            <td class="text-xs-right">
+              <v-btn fab dark small primary v-on:click.prevent="deleteSite(site.item.id)" type="submit">
+                <v-icon dark> remove </v-icon>
+              </v-btn>
+            </td>
+            <td class="text-xs-right">
+              <v-dialog v-model="dialog" width="50%">
+                <v-btn primary dark slot="activator">
+                  <v-icon dark> update</v-icon>
                 </v-btn>
-              </td>
-              <td class="text-xs-right">
-                <v-dialog width="50%">
-                  <v-btn primary dark slot="activator">
-                    <v-icon dark> update</v-icon>
-                  </v-btn>
-                  <v-card>
-                    <v-card-title>
-                      <span class="headline"> Atualizar Site </span>
-                    </v-card-title>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline"> Atualizar Site </span>
+                  </v-card-title>
 
-                    <v-card-text>
-                      <v-container grid-list-md>
-                        <form>
-                          <v-layout row>
-                            <v-flex xs12>
-                              <v-text-field
-                              label="Nome do site"
-                              v-model="site.item.name">
-                              </v-text-field>
-                              <v-text-field
-                              label="Latitude"
-                              v-model="site.item.lattitude"
-                              type="number">
-                              </v-text-field>
-                              <v-text-field
-                              label="Longitude"
-                              v-model="site.item.longitude"
-                              type="number">
-                              </v-text-field>
-                              <v-text-field
-                                label="Banda larga do site"
-                                v-model="site.item.bandwidth"
-                                type="number"
-                                min="0">
-                              </v-text-field>
-                              <v-select v-bind:items="ipalist" v-model="site.item.ipa_code" label="Qual a IPA do site?"item-text="name" bottom></v-select>
-                              <v-select v-bind:items="site_types" v-model="site.item.site_type" label="Tipo do site?"item-text="description" bottom></v-select>
-                            </v-flex>
-                          </v-layout>
-                        </form>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn class="blue--text darken-1" flat @click.native="dialog=false">Fechar</v-btn>
-                      <v-btn class="blue--text darken-1" v-on:click.prevent="updateSite(site.item)"
-                type="submit" flat @click.native="dialog=false">Salvar alterações</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </td>
-            </template>
-          </v-data-table>
-        </v-container>
+                  <v-card-text>
+                    <v-container grid-list-md>
+                      <form>
+                        <v-layout row>
+                          <v-flex xs12>
+                            <v-text-field label="Nome do site" v-model="site.item.name">
+                            </v-text-field>
+                            <v-text-field label="Latitude" v-model="site.item.lattitude" type="number">
+                            </v-text-field>
+                            <v-text-field label="Longitude" v-model="site.item.longitude" type="number">
+                            </v-text-field>
+                            <v-text-field label="Banda larga do site" v-model="site.item.bandwidth" type="number" min="0">
+                            </v-text-field>
+                            <v-select v-bind:items="ipalist" v-model="site.item.ipa_code" label="Qual a IPA do site?" item-text="name" bottom></v-select>
+                            <v-select v-bind:items="site_types" v-model="site.item.site_type" label="Tipo do site?" item-text="description" bottom></v-select>
+                          </v-flex>
+                        </v-layout>
+                      </form>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn class="blue--text darken-1" flat="flat" @click.native="dialog = false">Fechar</v-btn>
+                    <v-btn class="blue--text darken-1" v-on:click.prevent="updateSite(site.item)" flat="flat" @click.native="dialog = false">Salvar alterações</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </td>
+          </template>
+        </v-data-table>
+      </v-container>
       <router-view></router-view>
     </v-app>
   </div>
@@ -107,6 +87,7 @@ export default {
     site_longitude: '',
     site_bandwidth: '',
     site_ipa_code: '',
+    dialog: false,
     site_type_site: '',
     actual_site: '',
     // ipa: {name: this.ipa_name, code: ipa_code},
@@ -157,10 +138,10 @@ export default {
     },
     delete(id) {
       axios.delete('http://localhost:8000/site/'.concat(id).concat('/'))
-      .then()
-      .catch((e) => {
-        this.errors.push(e);
-      });
+        .then()
+        .catch((e) => {
+          this.errors.push(e);
+        });
     },
     put(site) {
       this.site_name = site.name;
@@ -175,21 +156,21 @@ export default {
         longitude: this.site_longitude,
         bandwidth: this.site_bandwidth,
         ipa_code: this.site_ipa_code,
-        site_type: this.site_type_site,
+        site_type: site.site_type.id,
       })
-     .then()
-     .catch((e) => {
-       this.errors.push(e);
-     });
+        .then()
+        .catch((e) => {
+          this.errors.push(e);
+        });
     },
     getSpecificSite(id) {
       axios.get('http://localhost:8000/site/'.concat(id).concat('/'))
-      .then((response) => {
-        this.actual_site = response.data;
-      })
-      .catch((e) => {
-        this.errors.push(e);
-      });
+        .then((response) => {
+          this.actual_site = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
     },
     getType() {
       axios.get('http://localhost:8000/sitetype/')
@@ -220,9 +201,11 @@ export default {
     },
     deleteSite(id) {
       this.delete(id);
+      this.getSites();
     },
     updateSite(site) {
       this.put(site);
+      this.getSites();
     },
     getIpaCode() {
       this.getIpa();
@@ -249,6 +232,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 .ipas {
   font-size: 18px;
   color: #FFFFF;
