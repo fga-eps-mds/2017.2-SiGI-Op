49a791afe33da3c81616272f26107bd32ac47c51
Harrison Pedro <hpedro1195@gmail.com>
2017-11-02 20:47:10 -0200

    Add dynamic markers

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -1,4 +1,7 @@
 <template>
+<div>
+   <v-btn class="blue--text darken-1" v-on:click.prevent="addMarkers" flat="flat">Adicionar Marcadores</v-btn>
+  
   <gmap-map
     :center="center"
     :zoom="7"
@@ -15,14 +18,23 @@
     ></gmap-marker>
     <gmap-polyline
       :key="index"
-      :path="polyline.path"
+      v-for="(p, index) in polylines"
+      :path="p.path"
     ></gmap-polyline>
   </gmap-map>
+  <ul>
+    <li v-for='item in objects'>
+      {{item}}
+    </li>
+    {{Object.keys(this.objects.site).length}}
+  </ul>
+</div>
 </template>
 
 <script>
 import * as VueGoogleMaps from 'vue2-google-maps';
 import Vue from 'vue';
+import axios from 'axios';
 
 Vue.use(VueGoogleMaps, {
   load: {
@@ -33,7 +45,7 @@ Vue.use(VueGoogleMaps, {
 export default {
   data() {
     return {
-      ipas: '',
+      objects: '',
       center: { lat: 10.0, lng: 10.0 },
       markers: [
         {
@@ -45,8 +57,40 @@ export default {
           position: { lat: 11.0, lng: 11.0 },
         },
       ],
-      polyline: { path: [{ lat: 10.0, lng: 10.0 }, { lat: 11.0, lng: 11.0 }] },
+      polylines: [
+        {
+          path: [{ lat: 10.0, lng: 10.0 }, { lat: 11.0, lng: 11.0 }],
+        },
+      ],
     };
+  },
+  methods: {
+    getObjects() {
+      axios
+        .get('http://localhost:8000/networkmap/')
+        .then((response) => {
+          // JSON responses are automatically parsed.
+          this.objects = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+    addMarkers() {
+      for (let i = 0; i < Object.keys(this.objects.site).length; i += 1) {
+        this.markers.push({
+          title: 'Teste'.concat(i),
+          position: { lat: this.objects.site.lattitude[i], lng: this.objects.site.longitude[i] },
+        });
+        // this.polylines.push({
+        //   path: Array.push({ lat: this.objects.site.lattitude[i],
+        //     lng: this.objects.site.longitude[i] }),
+        // });
+      }
+    },
+  },
+  created() {
+    this.getObjects();
   },
 };
 </script>
