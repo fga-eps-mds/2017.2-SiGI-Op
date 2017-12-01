e323d09a930b0dd0406d838bdcd5d7af15932173
Felipe Osório <felipe.osorio72@outlook.com.br>
2017-12-01 11:17:37 -0200

    Add generate pdf button in IPA

--- src/components/Ipas/Ipas.vue
+++ src/components/Ipas/Ipas.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="ipa">
+  <div class="ipa" id="content">
       <modal></modal>
       <data-table></data-table>
+      <button v-on:click="click">Generate PDF</button>
   </div>
 </template>
 
+<script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
 <script>
+import * as JsPDF from 'jspdf';
 import Modal from '../Modal';
 import DataTable from '../DataList';
+
+const doc = new JsPDF();
+const specialElementHandlers = {
+  '#editor': function editor(element, renderer) { // eslint-disable-line no-unused-vars
+    return true;
+  },
+};
+global.jQuery = require('jquery');
+
+const $ = global.jQuery;
 
 export default {
   name: 'ipa',
@@ -27,6 +40,15 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    click: function click(event) { // eslint-disable-line no-unused-vars
+      doc.fromHTML($('#content').html(), 15, 15, {
+        width: 170,
+        elementHandlers: specialElementHandlers,
+      });
+      doc.save('sample-file.pdf');
+    },
   },
   components: { Modal, DataTable },
   created() {
