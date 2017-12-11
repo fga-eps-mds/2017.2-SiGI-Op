c5b5f9d6ab08745ac2cceffb2785c69236093755
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2017-12-11 11:33:36 -0200

    Add test to uplink

---
+++ test/unit/specs/Uplink.spec.js
@@ -0,0 +1,35 @@
+// import moxios from 'moxios';
+// import { equal } from 'assert';
+import { mount } from 'vue-test-utils';
+import store from '@/vuex/store';
+import Uplink from '../../../src/components/Uplink/Uplink';
+
+describe('Uplink', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(Uplink, { store });
+  });
+
+  it('check default Uplink values', () => {
+    expect(typeof Uplink.data).to.equal('function');
+    const defaultData = Uplink.data();
+    expect(defaultData.headers[0].text).to.equal('ID');
+    expect(defaultData.headers[1].text).to.equal('Nome VLan');
+    expect(defaultData.headers[2].text).to.equal('Banda');
+    expect(defaultData.headers[3].text).to.equal('Código');
+  });
+
+  it('check default Uplink components', () => {
+    expect(typeof Uplink.components).to.equal('object');
+  });
+
+  it('check default Uplink creation', () => {
+    expect(typeof Uplink.created).to.equal('function');
+  });
+  it('renders a div', () => {
+    expect(wrapper.contains('div')).to.equal(true);
+    expect(wrapper.contains('.segment')).to.equal(true);
+  });
+});
+
